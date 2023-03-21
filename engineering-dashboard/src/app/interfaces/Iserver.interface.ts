6766a8679e273afd27e50879e187aac761d4a1a0
NEX1OQP <joseph.avallone@pb.com>
2023-03-21 12:58:48 -0400

    delete old files

--- engineering-dashboard/src/app/interfaces/Iserver.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Iserver {
-  id: number;
-  name: string;        // Server Name 
-  allocate:string;     // Name of person server is allocated to
-  status: string;      // Status of the server
-  redirectUrl:string;  // Redirect url
-  instanceId:string;   // server instance id
-}
