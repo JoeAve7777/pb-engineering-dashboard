1f25c06c2585f2dac760eaf8df103969351b8fd3
NEX1OQP <joseph.avallone@pb.com>
2023-04-04 13:34:59 -0400

    worked on charting stub screen

--- engineering-dashboard/src/app/screens/home/card-screen-view/bar-chart-widget/bar-chart-widget.component.ts
+++ engineering-dashboard/src/app/screens/home/card-screen-view/bar-chart-widget/bar-chart-widget.component.ts
@@ -1,41 +1,84 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  EventEmitter,
+  Output,
+} from "@angular/core";
+
+import { Subscription } from "rxjs";
+import { DashboardCardItem } from "../../dashboard/dashboard-card-item.model";
 import { PbdsDatavizLine } from 'pb-design-system/dataviz';
 
+
 @Component({
-    selector: "app-bar-chart-widget",
-    templateUrl: "./bar-chart-widget.component.html",
-    styleUrls: ["./bar-chart-widget.component.scss"],
+  selector: "app-bar-chart-widget",
+  templateUrl: "./bar-chart-widget.component.html",
+  styleUrls: ["./bar-chart-widget.component.scss"],
 })
-export class BarChartWidgetComponent implements OnInit {
-    constructor() {}
+export class BarChartWidgetComponent implements OnInit, OnDestroy {
+  @Input() dashboardCardItem: DashboardCardItem;
+  @Output() dataLoadCompleted = new EventEmitter<string>();
+  
+  barChartOb$!: Subscription;
 
-    medium: PbdsDatavizLine = {
-      labels: [
-        '2016-01-01T05:00:00.000Z',
-        '2017-01-01T05:00:00.000Z',
-        '2018-05-16T05:00:00.000Z',
-        '2019-07-23T05:00:00.000Z',
-        '2020-09-27T05:00:00.000Z'
-      ],
-      series: [
-        {
-          label: 'Dec 10th',
-          values: [6159, 3585, 3686, 3321, 6697]
-        },
-        {
-          label: 'Dec 17th',
-          values: [4161, 3529, 6455, 5092, 2726]
-        },
-        {
-          label: 'Dec 29',
-          values: [2508, 6323, 2545, 4101, 5386]
-        }
-      ]
-    };
-    ngOnInit(): void {}
+  screenCaption: string = "Since 1 week ago";
 
-    onHover(event)
-    {
+  medium: PbdsDatavizLine = {
+    labels: [
+      '2016-01-01T05:00:00.000Z',
+      '2017-01-01T05:00:00.000Z',
+      '2018-05-16T05:00:00.000Z',
+      '2019-07-23T05:00:00.000Z',
+      '2020-09-27T05:00:00.000Z'
+    ],
+    series: [
+      {
+        label: 'Dec 10th',
+        values: [6159, 3585, 3686, 3321, 6697]
+      },
+      {
+        label: 'Dec 17th',
+        values: [4161, 3529, 6455, 5092, 2726]
+      },
+      {
+        label: 'Dec 29',
+        values: [2508, 6323, 2545, 4101, 5386]
+      }
+    ]
+  };
 
-    }
+  constructor() {}
+
+  ngOnInit(): void {
+      this.loadData();
+  }
+
+  loadData() {
+      this.dataLoadCompleted.emit();
+  }
+
+  ngOnDestroy(): void {
+      if (
+          this.barChartOb$ != null &&
+          this.barChartOb$ != undefined
+      ) {
+          this.barChartOb$.unsubscribe();
+      }
+  }
+
+  onPrint() {
+      
+      let cardHtlm = document.getElementById(
+          "card-id-" + this.dashboardCardItem.id
+      ).innerHTML;
+      
+      document.getElementById("print-div").innerHTML = cardHtlm;
+
+      window.print();
+
+      return false;
+  }
 }
+
