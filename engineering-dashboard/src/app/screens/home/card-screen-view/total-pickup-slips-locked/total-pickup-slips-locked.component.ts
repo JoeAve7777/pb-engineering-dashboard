845bfa84a9e9f3a4340923cf6d0796de0bb58792
NEX1OQP <joseph.avallone@pb.com>
2023-04-20 10:17:34 -0400

    Worked on pickslip screens

--- engineering-dashboard/src/app/screens/home/card-screen-view/total-pickup-slips-locked/total-pickup-slips-locked.component.ts
+++ engineering-dashboard/src/app/screens/home/card-screen-view/total-pickup-slips-locked/total-pickup-slips-locked.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  EventEmitter,
+  Output,
+} from "@angular/core";
+
+import { Subscription } from "rxjs";
+import { DashboardCardItem } from "../../dashboard/dashboard-card-item.model";
 
 @Component({
   selector: 'app-total-pickup-slips-locked',
   templateUrl: './total-pickup-slips-locked.component.html',
   styleUrls: ['./total-pickup-slips-locked.component.scss']
 })
-export class TotalPickupSlipsLockedComponent implements OnInit {
+export class TotalPickupSlipsLockedComponent implements OnInit, OnDestroy {
+  @Input() dashboardCardItem: DashboardCardItem;
+  @Output() dataLoadCompleted = new EventEmitter<string>();
 
-  constructor() { }
+  Ob$!: Subscription;
 
-  ngOnInit() {
+  screenCaption: string = "Since 1 month ago";
+
+  constructor() {}
+
+  ngOnInit(): void {
+      this.loadData();
   }
 
+  loadData() {
+      this.dataLoadCompleted.emit();
+  }
+
+  ngOnDestroy(): void {
+      if (
+          this.Ob$ != null &&
+          this.Ob$ != undefined
+      ) {
+          this.Ob$.unsubscribe();
+      }
+  }
+
+  onPrint() {
+      
+      let cardHtlm = document.getElementById(
+          "card-id-" + this.dashboardCardItem.id
+      ).innerHTML;
+      
+      document.getElementById("print-div").innerHTML = cardHtlm;
+
+      window.print();
+
+      return false;
+  }
 }
+
+
