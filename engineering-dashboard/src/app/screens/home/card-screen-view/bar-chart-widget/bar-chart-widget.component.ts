90ea035ee5b39b8e2a11626f1be5a0ae90e74735
JoeAve7777 <37075387+JoeAve7777@users.noreply.github.com>
2023-05-23 19:40:05 -0400

    worked on chart

--- engineering-dashboard/src/app/screens/home/card-screen-view/bar-chart-widget/bar-chart-widget.component.ts
+++ engineering-dashboard/src/app/screens/home/card-screen-view/bar-chart-widget/bar-chart-widget.component.ts
@@ -22,36 +22,35 @@ export class BarChartWidgetComponent implements OnInit, OnDestroy {
     @Input() dashboardCardItem: DashboardCardItem;
     @Output() dataLoadCompleted = new EventEmitter<string>();
 
-    //@ViewChild('cm') contextMenu: any;
     @ViewChild('chartCanvas') chartCanvas: any;
 
     barChartOb$!: Subscription;
 
     screenCaption: string = "Since 1 week ago";
 
-    medium: PbdsDatavizLine = {
-        labels: [
-            '2016-01-01T05:00:00.000Z',
-            '2017-01-01T05:00:00.000Z',
-            '2018-05-16T05:00:00.000Z',
-            '2019-07-23T05:00:00.000Z',
-            '2020-09-27T05:00:00.000Z'
-        ],
-        series: [
-            {
-                label: 'Dec 10th',
-                values: [6159, 3585, 3686, 3321, 6697]
-            },
-            {
-                label: 'Dec 17th',
-                values: [4161, 3529, 6455, 5092, 2726]
-            },
-            {
-                label: 'Dec 29',
-                values: [2508, 6323, 2545, 4101, 5386]
-            }
-        ]
-    };
+    // medium: PbdsDatavizLine = {
+    //     labels: [
+    //         '2016-01-01T05:00:00.000Z',
+    //         '2017-01-01T05:00:00.000Z',
+    //         '2018-05-16T05:00:00.000Z',
+    //         '2019-07-23T05:00:00.000Z',
+    //         '2020-09-27T05:00:00.000Z'
+    //     ],
+    //     series: [
+    //         {
+    //             label: 'Dec 10th',
+    //             values: [6159, 3585, 3686, 3321, 6697]
+    //         },
+    //         {
+    //             label: 'Dec 17th',
+    //             values: [4161, 3529, 6455, 5092, 2726]
+    //         },
+    //         {
+    //             label: 'Dec 29',
+    //             values: [2508, 6323, 2545, 4101, 5386]
+    //         }
+    //     ]
+    // };
 
     constructor() { }
 
@@ -72,36 +71,70 @@ export class BarChartWidgetComponent implements OnInit, OnDestroy {
         }
     }
   
-    data: ChartData = {
-        labels: ['WWW 123', 'February', 'March', 'April', 'May', 'June', 'July'],
+    data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [
             {
                 label: this.screenCaption,
-                data: [65, 59, 80, 81, 56, 55, 40]
+                fill: false,
+                borderColor: getComputedStyle(document.documentElement).getPropertyValue('--h1_color'),
+                yAxisID: 'y',
+                tension: 0.4,
+                data: [65, 59, 80, 81, 56, 55, 10]
+            },
+            {
+                label: this.screenCaption,
+                fill: false,
+                borderColor: getComputedStyle(document.documentElement).getPropertyValue('--blue'),
+                tension: 0.4,
+                data: [28, 48, 40, 19, 86, 27, 90]
             }
         ]
     };
 
- /*    options: ChartOptions = {
-        responsive: true,
-        maintainAspectRatio: false
-    };
-
-    contextMenuModel = [
-        {
-            label: 'Copy Image Paul',
-            icon: 'pi pi-copy',
-            command: () => {          
-                alert('contextMenuModel');      
-              //  this.copyImage();
+    options: ChartOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+            legend: {
+                labels: {
+                    color: 'black'
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: 'black'
+                },
+                grid: {
+                    color: 'black'
+                }
+            },
+            y: {
+                type: 'linear',
+                display: true,
+                position: 'left',
+                ticks: {
+                    color: 'black'
+                },
+                grid: {
+                    color: 'black'
+                }
+            },
+            y1: {
+                type: 'linear',
+                display: true,
+                position: 'right',
+                ticks: {
+                    color: 'black'
+                },
+                grid: {
+                    drawOnChartArea: false,
+                    color: 'black'
+                }
             }
         }
-    ];
-
-    showContextMenu(event: MouseEvent) {
-        event.preventDefault();
-        this.contextMenu.show(event);
-    } */
-
+    };
 }
 
