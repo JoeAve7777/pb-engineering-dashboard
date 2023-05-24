0f54620ef5e0e149b0c8479b695e89360867d74c
JoeAve7777 <37075387+JoeAve7777@users.noreply.github.com>
2023-05-23 21:29:36 -0400

    Worked on Successful Logins

--- engineering-dashboard/src/app/screens/home/card-screen-view/bar-chart-widget/bar-chart-widget.component.ts
+++ engineering-dashboard/src/app/screens/home/card-screen-view/bar-chart-widget/bar-chart-widget.component.ts
@@ -10,7 +10,6 @@ import {
 
 import { Subscription } from "rxjs";
 import { DashboardCardItem } from "../../dashboard/dashboard-card-item.model";
-import { PbdsDatavizLine } from 'pb-design-system/dataviz';
 import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
@@ -26,32 +25,8 @@ export class BarChartWidgetComponent implements OnInit, OnDestroy {
 
     barChartOb$!: Subscription;
 
-    screenCaption: string = "Since 1 week ago";
-
-    // medium: PbdsDatavizLine = {
-    //     labels: [
-    //         '2016-01-01T05:00:00.000Z',
-    //         '2017-01-01T05:00:00.000Z',
-    //         '2018-05-16T05:00:00.000Z',
-    //         '2019-07-23T05:00:00.000Z',
-    //         '2020-09-27T05:00:00.000Z'
-    //     ],
-    //     series: [
-    //         {
-    //             label: 'Dec 10th',
-    //             values: [6159, 3585, 3686, 3321, 6697]
-    //         },
-    //         {
-    //             label: 'Dec 17th',
-    //             values: [4161, 3529, 6455, 5092, 2726]
-    //         },
-    //         {
-    //             label: 'Dec 29',
-    //             values: [2508, 6323, 2545, 4101, 5386]
-    //         }
-    //     ]
-    // };
-
+    screenCaption: string = "Since 1 month ago";
+ 
     constructor() { }
 
     ngOnInit(): void {
@@ -72,68 +47,35 @@ export class BarChartWidgetComponent implements OnInit, OnDestroy {
     }
   
     data = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['Dec 10, 4:00pm', 'Dec 17,4:00pm', 'Dec 24, 4:00pm', 'Dec 31, 4:00pm', 'Jan 7, 4:00pm'],
         datasets: [
             {
                 label: this.screenCaption,
                 fill: false,
                 borderColor: getComputedStyle(document.documentElement).getPropertyValue('--h1_color'),
+                //borderColor: getComputedStyle(document.documentElement).getPropertyValue('--blue'),
                 yAxisID: 'y',
                 tension: 0.4,
-                data: [65, 59, 80, 81, 56, 55, 10]
-            },
-            {
-                label: this.screenCaption,
-                fill: false,
-                borderColor: getComputedStyle(document.documentElement).getPropertyValue('--blue'),
-                tension: 0.4,
-                data: [28, 48, 40, 19, 86, 27, 90]
+                data: [10,1,20,35,51]
             }
+            ,
+            // {
+            //     label: this.screenCaption,
+            //     fill: false,
+            //     borderColor: getComputedStyle(document.documentElement).getPropertyValue('--text'),
+            //     tension: 0.4,
+            //     data: [28, 48, 40, 19, 86, 27, 90]
+            // }
         ]
     };
 
-    options: ChartOptions = {
-        maintainAspectRatio: false,
-        aspectRatio: 0.6,
+     options: ChartOptions = {
         plugins: {
-            legend: {
-                labels: {
-                    color: 'black'
-                }
-            }
-        },
-        scales: {
-            x: {
-                ticks: {
-                    color: 'black'
-                },
-                grid: {
-                    color: 'black'
-                }
-            },
-            y: {
-                type: 'linear',
+            title: {
                 display: true,
-                position: 'left',
-                ticks: {
-                    color: 'black'
-                },
-                grid: {
-                    color: 'black'
-                }
-            },
-            y1: {
-                type: 'linear',
-                display: true,
-                position: 'right',
-                ticks: {
-                    color: 'black'
-                },
-                grid: {
-                    drawOnChartArea: false,
-                    color: 'black'
-                }
-            }
+                text:'Successful Logins',
+                color: getComputedStyle(document.documentElement).getPropertyValue('--text')
+              }
         }
     };
 }
